6670c1908fa4df3374cb739bf17d1b4c8bd64812
silabantu <112931609+silabantu@users.noreply.github.com>
2022-11-11 09:15:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -21,9 +21,9 @@ global.APIKeys = {
 // Other
 global.owner = ['6282287219167']
 global.ownernomer = "6282287219167"
-global.premium = ['6282287219167']
-global.packname = 'Sticker By'
-global.author = 'Sad-Bot'
+global.premium = ['994403231195']
+global.packname = 'jonkris bot'
+global.author = 'kristian-Bot'
 global.sessionName = 'papah'  //jangan diganti bro nanti error
 global.jumlha = '999'
 global.jumhal = '100000000000000'
